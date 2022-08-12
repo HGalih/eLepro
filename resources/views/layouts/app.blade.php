b4db25472eb457cc85627490c4e7d30cdaa2c55e
rama-adi <17980909+rama-adi@users.noreply.github.com>
2022-08-12 10:46:18 +0700

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen" style="background-image: url('{{asset('background.jpeg')}}')">
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{asset('elepro.png')}}" alt="" class="h-16 w-16" {{$attributes}}>
+</a>
